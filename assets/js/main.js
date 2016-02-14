11620cc8dce73db0c0228a06f5ec88486668c234
Keita Yagi <kei.yagi@gmail.com>
2016-02-14 14:09:14 +0900

    add Stat.js and add some 3d objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,17 +92,148 @@
   	return Math.sqrt(x*x + y*y);
   };
 
-  var field,
+  var stats = new Stats();
+  stats.setMode(0); // 0: fps, 1: ms, 2: mb
+
+  // align top-left
+  stats.domElement.style.position = 'fixed';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
+
+  var cvs,
       camera,
       trackball,
       renderer,
       scene,
-      people_cnt = 20,
-      people = [],
-      place_mesh,
-      wall_mesh;
+      floor_pos = [-1010, -510, -10],
+      floor = [],
+      obstacle,
+      elevator = [],
+      elevator_cnt = 6,
+      people_cnt = 5,
+      people = [];
 
-  // person
+  function setGl(){
+  	cvs = document.getElementById('field');
+  	// scene
+  	scene = new THREE.Scene();
+  	// camera
+  	var width = 1100;
+  	var height = 2000;
+  	var fov = 60;
+  	var aspect = width/height;
+  	var near = 1;
+  	var far = 10000;
+  	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  	camera.position.set(0, 0, 50);
+
+  	// trackball
+  	trackball = new THREE.TrackballControls(camera);
+  	trackball.noRotate = false;
+  	trackball.rotateSpeed = 1.0;
+  	trackball.noZoom = false;
+  	trackball.zoomSpeed = 1.0;
+  	trackball.noPan = false;
+  	trackball.panSpeed = 1.0;
+  	trackball.staticMoving = true;
+  	trackball.dynamicDampingFactor = 0.3;
+
+  	// renderer
+  	renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    renderer.setClearColor(new THREE.Color(0xffffff));
+  	cvs.appendChild(renderer.domElement);
+
+  	// light
+  	var directionalLight = new THREE.DirectionalLight(0xffffff);
+  	directionalLight.position.set(0, 0.7, 0.7);
+  	scene.add(directionalLight);
+  }
+
+  // field object
+  function Field(size, position){
+  	this.size = size;
+  	this.position = position;
+  	this.init();
+  }
+  Field.prototype = {
+  	init: function(){
+  	  var me = this;  		
+      var geo = new THREE.PlaneGeometry(me.size.w, me.size.h, 32);
+      var mat = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide, transparent: true, opacity: 0.5});
+      var mesh = new THREE.Mesh(geo, mat);
+      var edge = new THREE.EdgesHelper(mesh, 0x000000);
+      me.o = mesh;
+      me.o.position.set(me.position.x, me.position.y, me.position.z);
+      edge.material.linewidth = 1;
+  	  scene.add(mesh);
+  	  scene.add(edge);
+  	}
+  };
+
+  // obstacle object
+  function Obstacle(size, position){
+  	this.size = size;
+  	this.position = position;
+  	this.init();
+  }
+  Obstacle.prototype = {
+  	init: function(){
+  	  var me = this;  		
+      var geo = new THREE.PlaneGeometry(me.size.w, me.size.h, 32);
+      var mat = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide, transparent: true, opacity: 0.5});
+      var mesh = new THREE.Mesh(geo, mat);
+      var edge = new THREE.EdgesHelper(mesh, 0x000000);
+      me.o = mesh;
+      me.o.position.set(me.position.x, me.position.y, me.position.z);
+      edge.material.linewidth = 1;
+  	  scene.add(mesh);
+  	  scene.add(edge);
+  	}
+  };
+
+  // obstacle object
+  function Elevator(size, position){
+  	this.size = size;
+  	this.floor = 0; // 0: 1f, 1: 2f, 2: 3f
+  	this.v = 10;
+  	this.pos = floor_pos;
+  	this.position = position;
+  	this.init();
+  }
+  Elevator.prototype = {
+  	init: function(){
+  	  var me = this;  		
+      var geo = new THREE.BoxGeometry(me.size.w, me.size.h, 32);
+      var mat = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide, transparent: true, opacity: 0.5});
+      var mesh = new THREE.Mesh(geo, mat);
+      var edge = new THREE.EdgesHelper(mesh, 0x000000);
+      me.o = mesh;
+      me.o.position.set(me.position.x, me.position.y, me.position.z);
+      edge.material.linewidth = 1;
+  	  scene.add(mesh);
+  	  scene.add(edge);
+  	},
+  	move: function(){
+  	  var me = this;
+  	  me.o.position.z += me.v;
+  	  // state
+  	  if(me.o.position.z <= me.pos[0]){
+  	  	me.floor = 0;
+  	  }else if(me.o.position.z >= me.pos[1] && me.o.position.z < me.pos[2]){
+  	  	me.floor = 1;
+  	  }else{
+        me.floor = 2;
+  	  }
+
+      if(me.o.position.z >= me.pos[2] + 20 || me.o.position.z <= me.pos[0]){
+      	me.v *= -1;
+      }
+  	}
+  };
+
+  // person object
   function Person(color, position, speed){
   	this.color = color[Math.floor(Math.random() * color.length)];
   	this.position = position;
@@ -122,10 +253,10 @@
       var me = this;
       me.o.position.x += me.speed.x;
       me.o.position.y += me.speed.y;
-      if(me.o.position.x >= 250 || me.o.position.x <= -250){
+      if(me.o.position.x >= 500 || me.o.position.x <= -500){
         me.speed.x *= -1;
       }
-      if(me.o.position.y >= 500 || me.o.position.y <= -500){
+      if(me.o.position.y >= 250 || me.o.position.y <= -250){
         me.speed.y *= -1;
       }
       // 衝突判定
@@ -133,10 +264,10 @@
     },
     touch: function(){
       var me = this;
-      if(me.o.position.x >= wall_mesh.position.x - 20 && (me.o.position.y >= wall_mesh.position.y -20 && me.o.position.y <= wall_mesh.position.y + 20)){
+      if(me.o.position.x >= obstacle.o.position.x - 20 && (me.o.position.y >= obstacle.o.position.y -20 && me.o.position.y <= obstacle.o.position.y + 20)){
       	me.speed.x = 0;
       	me.speed.y = 1;
-      }else if(me.o.position.y >= wall_mesh.position.y -20 && (me.o.position.x >= wall_mesh.position.x -20 && me.o.position.x <= wall_mesh.position.x + 20)){
+      }else if(me.o.position.y >= obstacle.o.position.y -20 && (me.o.position.x >= obstacle.o.position.x -20 && me.o.position.x <= obstacle.o.position.x + 20)){
       	me.speed.x = 1;
       	me.speed.y = 0;
       }
@@ -145,60 +276,22 @@
 
   // init
   function init(){
+  	stats.begin();
   	// Three.js setting
-  	field = document.getElementById('field');
+  	setGl();
 
-  	// scene
-  	scene = new THREE.Scene();
+  	// Three.js object
 
-  	// camera
-  	var width = 1100;
-  	var height = 600;
-  	var fov = 60;
-  	var aspect = width/height;
-  	var near = 1;
-  	var far = 2000;
-  	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  	camera.position.set(0, 0, 50);
-
-  	// trackball
-  	trackball = new THREE.TrackballControls(camera);
-  	trackball.noRotate = false;
-  	trackball.rotateSpeed = 1.0;
-  	trackball.noZoom = false;
-  	trackball.zoomSpeed = 1.0;
-  	trackball.noPan = false;
-  	trackball.panSpeed = 1.0;
-  	trackball.staticMoving = true;
-  	trackball.dynamicDampingFactor = 0.3;
-
-  	// renderer
-  	renderer = new THREE.WebGLRenderer();
-  	renderer.setSize(width, height);
-  	field.appendChild(renderer.domElement);
-
-  	// light
-  	var directionalLight = new THREE.DirectionalLight(0xffffff);
-  	directionalLight.position.set(0, 0.7, 0.7);
-  	scene.add(directionalLight);
-
-  	// geometry
-
-  	var place = new THREE.PlaneGeometry(500, 1000, 32);
-  	var place_mat = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide});
-  	var wall = new THREE.PlaneGeometry(20, 20, 32);
-  	var wall_mat = new THREE.MeshBasicMaterial({color: 0xccccc, side: THREE.DoubleSide});
-  	place_mesh = new THREE.Mesh(place, place_mat);
-  	place_mesh.position.z = -10;
-  	scene.add(place_mesh);
-  	wall_mesh = new THREE.Mesh(wall, wall_mat);
-  	wall_mesh.position.x = 100;
-  	wall_mesh.position.y = 0;
-  	scene.add(wall_mesh);
+  	for(var i = 0, cnt = floor_pos.length; i < cnt; i++){
+      floor[i] = new Field({w: 1000, h: 500}, {x: 0, y: 0, z: floor_pos[i]});
+  	}
+  	for(var i = 0; i < elevator_cnt; i++){
+      elevator[i] = new Elevator({w: 60, h: 60}, {x: 200 + i*60, y: 200, z: Math.floor(Math.random()*-1000)});
+  	}
+    obstacle = new Obstacle({w: 30, h: 30}, {x: 100, y: 0, z: -5});
   	for(var i = 0; i < people_cnt; i++){
   	  people[i] = new Person([0xff0000,0x00ff00,0x0000ff], {x: 0, y:0}, {x: Math.floor(Math.random()*4)-2, y: Math.floor(Math.random()*4)-2});
   	}
-
     animate();
 
   	/*
@@ -217,11 +310,17 @@
   }
 
   function animate(){
+    stats.begin();
   	requestAnimationFrame(animate);
   	trackball.update();
   	for(var i = 0; i < people_cnt; i++){
   	  people[i].move();
   	}
+  	for(var i = 0; i < elevator_cnt; i++){
+      elevator[i].move();
+    }
+  	console.log(camera.position.x + ':' + camera.position.y + ':' + camera.position.z);
   	renderer.render(scene, camera);
+  	stats.end();
   }
 })();
